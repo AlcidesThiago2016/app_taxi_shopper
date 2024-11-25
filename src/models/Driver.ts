c1b41a0927a765bcf144884a8b6b1f6a3150d77f
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-11-25 14:29:58 -0300

    Create entity and file Driver.ts

---
+++ src/models/Driver.ts
@@ -0,0 +1,53 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../database/config";
+
+class Driver extends Model {
+    public id!: number;
+    public nome!: string;
+    public description!: string;
+    public car!: string;
+    public rating!: number;
+    public rate!: number;
+    public minKm!: number;
+}
+
+Driver.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        car: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            defaultValue: 0
+        },
+        rate: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        minKm: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        tableName: "drivers"
+    }
+);
+
+export default Driver;
