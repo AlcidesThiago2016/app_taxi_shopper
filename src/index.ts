6eafc7654adbf72ddafb9ac92efc01a5b71ca0ca
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-11-25 16:35:25 -0300

    Add routes index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
+import express from "express";
 import sequelize from "./database/config";
-import Driver from "./models/driver";
+import driverRoutes from "./routes/driverRoutes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/drivers", driverRoutes);
 
 const databaseStart = async () => {
     try{
         await sequelize.authenticate();
         console.log("Conexão realizada com sucesso.");
-        await sequelize.sync({force: true});
-        console.log("Tabelas sincronizadas com sucesso!");
+        await sequelize.sync();
+        console.log("Banco de Dados sincronizado com sucesso!");
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Servidor Inicializado com sucesso, Rodando na porta ${PORT}`);
+        });
     } catch (error){
         console.error("Falha na conexão com o Banco de Dados: ", error);
     }
 };
+
 databaseStart();
