5b7d07a104f72c654044e669b3e66b8e466acfe5
Rickson Passos <rpc2307@gmail.com>
2024-11-28 00:12:48 -0300

    remove all comments

--- Front-End/vite.config.ts
+++ Front-End/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:3000", // URL do BackEnd
+                target: "http://localhost:3000",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
